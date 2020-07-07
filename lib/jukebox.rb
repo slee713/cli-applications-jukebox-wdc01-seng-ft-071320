14cdb289d369b96e16a4c865804a6f1d44965b2c
Steven Lee <steven.lee1393@gmail.com>
2020-07-07 22:02:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,16 +1,5 @@
 # Add your code here
 require 'pry'
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
 
 def help
   puts "I accept the following commands:"
@@ -49,3 +38,16 @@ end
 def exit_jukebox
   puts "Goodbye"
 end
+
+def runs
+  puts "Please enter a command:"
+  user_input = gets.strip
+  if user_input == "help"
+    help
+  elsif user_input == "list"
+    list
+  elsif user_input == "play"
+    play
+  elsif user_input == "exit"
+    exit_jukebox
+  else
