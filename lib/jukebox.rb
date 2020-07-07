5e2bf62b07c5997ba7488de38a7beaf53d81fb3a
Steven Lee <steven.lee1393@gmail.com>
2020-07-07 21:32:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,17 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help: displays this help message"
+  puts "- list: displays a list of songs you can play"
+  puts "- play: lets you choose a song to play"
+  puts "- exit: exits this program"
+end
+
+def list(songs)
+  songs.each_with_index {|index, item|
+    puts "#{index +1}. #{item}"
+  }
+end
