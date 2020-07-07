63e14d877e912a300b0043c7c56005fc0fcd5be2
Steven Lee <steven.lee1393@gmail.com>
2020-07-07 21:44:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,7 +36,7 @@ def play(songs)
     song_name.push(item)
   end
 
-  binding.pry
+  # binding.pry
   if users_input.class == Integer && song_number.include? users_input
     puts "Playing #{song_name[users.input-1]}"
   elsif users_input.class == String && song_name.include? users_input
@@ -44,5 +44,5 @@ def play(songs)
   else
     puts "Invalid input, please try again"
   end
-  
+
 end
